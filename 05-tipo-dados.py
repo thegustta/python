05fb983ab31ccfccc8b83cedd841e56cc7cd60ef
Gustavo Henrique <guhzerahssilva@gmail.com>
2024-09-10 10:40:57 -0300

    Add files via upload

---
+++ 05-tipo-dados.py
@@ -0,0 +1,13 @@
+# Tipos de dados em Pyton
+# Inteiros
+inteiro = 10
+print(inteiro)
+# Floats
+float = 10.5
+print(float)
+# Strings
+string = "Olá, mundo!"
+print(string)
+# Booleeanos
+booleano = True
+print(booleano)
