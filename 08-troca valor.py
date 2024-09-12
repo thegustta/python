8a051c8c8840697d8edac6271a9f573ce9925a2a
Gustavo Henrique <guhzerahssilva@gmail.com>
2024-09-12 11:55:57 -0300

    Add files via upload

---
+++ 08-troca valor.py
@@ -0,0 +1,16 @@
+# Armazenar os valores iniciais
+A = 5
+B = 10
+
+# Exibir os valores iniciais
+print("Valores iniciais:")
+print("A =", A)
+print("B =", B)
+
+# Trocar os valores de A e B
+A, B = B, A
+
+# Exibir os valores após a troca
+print("\nValores após a troca:")
+print("A =", A)
+print("B =", B)
