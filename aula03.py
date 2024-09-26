0bcfe5b3cbab840f06170b85a2e79bb51c97aa5f
Gustavo Henrique <guhzerahssilva@gmail.com>
2024-09-26 10:57:19 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,16 @@
+#aula 03
+# Não ha necessidade de declara-las no inicio do programa
+# Basta criar no meio do algoritmo com a atribuição
+
+teste = "Gustavo"
+print(teste) # Imprime a string "Gustavo"
+print(type(teste))  # Imprime o tipo da variável, string
+teste = 10
+print(teste)  # Imprime o número 10
+print(type(teste))  # Imprime o tipo da variável, que é int
+teste = 1.80
+print(teste)  # Imprime o número 1.80
+print(type(teste))  # Imprime o tipo da variável, que é float
+teste = True
+print(teste)  # Imprime o valor True
+print(type(teste))  # Imprime o tipo da variável, que é bool
