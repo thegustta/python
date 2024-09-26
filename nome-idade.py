7eed40ec7a501a45e476c62f87ac9b624a8089bd
Gustavo Henrique <guhzerahssilva@gmail.com>
2024-09-26 10:24:14 -0300

    Add files via upload

---
+++ nome-idade.py
@@ -0,0 +1,15 @@
+# Nome-idade
+# atribuição de variáveis
+nome = "Gusta\n"
+idade = 16
+altura = 1,80
+
+#exibir mensagem na telaa
+print(" nome: ", nome," idade: ", idade, " altura : ", altura)
+#formação de string
+print(f"nome: {nome} idade: {idade} altura: {altura }")
+nome = (input("Digite o seu nome: "))
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite a sua altura: "))
+
+print(f"nome: {nome}idade: {idade} altura: {altura}")
